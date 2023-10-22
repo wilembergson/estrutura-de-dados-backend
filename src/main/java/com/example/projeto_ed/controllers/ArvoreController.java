3a8f2a257e6aa09bc71d1b2a9ee2c186c43e24ff
Wilembergson Alfredo da Silva <56082029+wilembergson@users.noreply.github.com>
2023-10-22 01:40:40 -0300

    adicionar dados teste

---
+++ src/main/java/com/example/projeto_ed/controllers/ArvoreController.java
@@ -0,0 +1,40 @@
+package com.example.projeto_ed.controllers;
+
+import com.example.projeto_ed.dto.NovoElementoDTO;
+import com.example.projeto_ed.estruturas.arvore.ABP;
+import com.example.projeto_ed.estruturas.arvore.No;
+import com.example.projeto_ed.estruturas.fila.Fila;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/arvore")
+public class ArvoreController {
+    private ABP arvore = new ABP();
+
+
+    @PostMapping("/adicionar")
+    public void adicionar(@RequestBody NovoElementoDTO elemento){
+        arvore.insere(10);
+        arvore.insere(6);
+        arvore.insere(22);
+        arvore.insere(15);
+        arvore.insere(2);
+        arvore.insere(8);
+        arvore.insere(3);
+        arvore.insere(35);
+        arvore.insere(16);
+        /*arvore.insere(14);
+        arvore.insere(9);
+        arvore.insere(36);
+        arvore.insere(34);
+        arvore.insere(1);
+        arvore.insere(7);*/
+    }
+
+    @GetMapping("/listar")
+    public No obterArvore(){
+        return arvore.exibeRaiz();
+    }
+}
---
+++ src/main/java/com/example/projeto_ed/estruturas/arvore/ABP.java
@@ -0,0 +1,141 @@
+package com.example.projeto_ed.estruturas.arvore;
+
+public class ABP {
+	private No raiz;
+	
+	public ABP(){
+		raiz = null;
+	}
+	
+	/** Verifica se a árvore está vazia */
+	public boolean vazia (){
+		return (raiz == null);
+	}
+
+	/**Buscar recursivamente a partir da raiz.
+	    Retorna o endereço se o elemento for
+	    encontrado, caso contrário retorna NULL*/
+	private No busca(No T, int valor) {
+		if (T == null)
+			return null;  // Arvore Vazia
+
+		if(T.getConteudo() == valor)
+			return T; 	// Elem. encontrado na raiz
+		
+		if (valor < T.getConteudo())
+			return busca(T.getEsq(), valor);
+	    else
+			return busca(T.getDir(), valor);
+	}
+	
+	/**Buscar um elemento na ABP
+    		Retorna o endereço se o elemento for
+    		encontrado, caso contrário retorna NULL*/
+	public No busca(int valor) {          
+		//if (raiz != null) 
+			return busca(raiz, valor);
+		
+		//return null;
+	}
+	
+	public No buscaIterativa(int valor) {          
+		if (vazia())
+			return null;
+		
+		No aux = raiz;
+		while (aux != null) {
+			// Verificando se o conteudo do no atual 
+			// é igual ao valor procurado
+			if (aux.getConteudo() == valor) {
+				return aux;
+			}
+			
+			// Se o valor procurado for menor que raiz,
+			// continue pesquisando na sub-arv da esq.
+			if (valor < aux.getConteudo()){
+				aux = aux.getEsq();
+			}
+			// Caso contratio, pesquise na sub-arv 
+			// da direita
+			else {
+				aux = aux.getDir();
+			}
+		}
+
+		return null;
+	}
+	
+	
+	/**Exibe o conteúdo de uma árvore no formato in-ordem
+	    (preserva a ordenação)*/
+	private void exibe (No T){
+		if (T != null) {
+			exibe(T.getEsq());
+			System.out.print(" " + T.getConteudo());
+			exibe(T.getDir());
+		}
+	}
+
+	public void exibe() {
+		if (raiz == null)
+			System.out.println("Arvore vazia");
+		else
+			exibe(raiz);
+	}
+
+	public No exibeRaiz (){
+		return raiz;
+	}
+
+	/**Exibe o conteúdo de uma árvore no formato in-ordem
+    (preserva a ordenação)*/
+	private void exibeDec(No T){
+		if (T != null) {
+			exibeDec(T.getDir());
+			System.out.print(" " + T.getConteudo());
+			exibeDec(T.getEsq());
+		}
+	}
+	
+	public void exibeDec() {
+		if (raiz == null)
+			System.out.println("Arvore vazia");
+		else
+			exibeDec(raiz);
+	}
+	
+	/**Insere um nó em uma árvore ABP
+	    Retorna 1 se a inserção for com sucesso.
+	    Caso contrário retorna 0*/
+	public boolean insere(int valor){
+
+		No novoNo = new No();
+		novoNo.setConteudo(valor);
+		novoNo.setEsq(null);
+		novoNo.setDir(null);
+
+		// Quando a arvore estiver vazia, o novoNó será a raiz da arv
+		if (raiz == null){ 
+	 		raiz = novoNo;
+			return true;
+		}
+
+	    // Procura a posicao correta pra inserir o novo no
+	    No aux = raiz;
+	    No p = null;
+	    while (aux != null) {
+	    	p = aux;
+			if (valor < aux.getConteudo())
+				aux = aux.getEsq();
+			else
+				aux = aux.getDir();
+		}
+
+		// Encontrou um nó folha para inserir
+		if (valor < p.getConteudo())
+			p.setEsq(novoNo);
+		else
+			p.setDir(novoNo);
+		return true;
+	}
+}
